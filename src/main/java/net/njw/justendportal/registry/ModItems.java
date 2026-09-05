65ab0276d03f25cdab22aec1f6d54c80d4ccd09f
JWN <89252111+skwodnjs@users.noreply.github.com>
2026-09-05 14:44:03 +0900

    Register custom End Portal Generator item

--- src/main/java/net/njw/justendportal/registry/ModItems.java
+++ src/main/java/net/njw/justendportal/registry/ModItems.java
@@ -1,13 +1,13 @@
 package net.njw.justendportal.registry;
 
-import net.minecraft.world.item.BlockItem;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.njw.justendportal.JustEndPortal;
+import net.njw.justendportal.item.EndPortalGeneratorItem;
 
 public final class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(JustEndPortal.MODID);
-    public static final DeferredItem<BlockItem> END_PORTAL_GENERATOR = ITEMS.registerSimpleBlockItem(ModBlocks.END_PORTAL_GENERATOR);
+    public static final DeferredItem<EndPortalGeneratorItem> END_PORTAL_GENERATOR = ITEMS.registerItem("end_portal_generator", properties -> new EndPortalGeneratorItem(ModBlocks.END_PORTAL_GENERATOR.get(), properties.stacksTo(1).useBlockDescriptionPrefix()));
 
     private ModItems() {
     }
