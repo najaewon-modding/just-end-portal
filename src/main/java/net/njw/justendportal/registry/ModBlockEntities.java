39641d9a4111ea9d2c1d80c162be1d1968eb1fb3
JWN <89252111+skwodnjs@users.noreply.github.com>
2026-09-05 20:08:16 +0900

    Register arrival platform block entity

--- src/main/java/net/njw/justendportal/registry/ModBlockEntities.java
+++ src/main/java/net/njw/justendportal/registry/ModBlockEntities.java
@@ -5,6 +5,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.njw.justendportal.JustEndPortal;
+import net.njw.justendportal.block.entity.ArrivalPlatformBlockEntity;
 import net.njw.justendportal.block.entity.EndPortalGeneratorBlockEntity;
 import net.njw.justendportal.block.entity.LinkedEndPortalBlockEntity;
 
@@ -12,6 +13,6 @@ public final class ModBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, JustEndPortal.MODID);
     public static final Supplier<BlockEntityType<EndPortalGeneratorBlockEntity>> END_PORTAL_GENERATOR = BLOCK_ENTITY_TYPES.register("end_portal_generator", () -> new BlockEntityType<>(EndPortalGeneratorBlockEntity::new, false, ModBlocks.END_PORTAL_GENERATOR.get()));
     public static final Supplier<BlockEntityType<LinkedEndPortalBlockEntity>> LINKED_END_PORTAL = BLOCK_ENTITY_TYPES.register("linked_end_portal", () -> new BlockEntityType<>(LinkedEndPortalBlockEntity::new, false, ModBlocks.LINKED_END_PORTAL.get()));
-
+    public static final Supplier<BlockEntityType<ArrivalPlatformBlockEntity>> ARRIVAL_PLATFORM = BLOCK_ENTITY_TYPES.register("arrival_platform", () -> new BlockEntityType<>(ArrivalPlatformBlockEntity::new, false, ModBlocks.ARRIVAL_PLATFORM.get()));
     private ModBlockEntities() {}
 }
