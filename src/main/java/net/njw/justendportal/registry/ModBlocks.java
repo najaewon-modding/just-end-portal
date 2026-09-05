cf0da3d9ee73c3a0d00a332d8b0d2d131c0c1074
JWN <89252111+skwodnjs@users.noreply.github.com>
2026-09-05 16:09:07 +0900

    Tune linked portal breaking behavior

--- src/main/java/net/njw/justendportal/registry/ModBlocks.java
+++ src/main/java/net/njw/justendportal/registry/ModBlocks.java
@@ -12,7 +12,7 @@ import net.njw.justendportal.block.LinkedEndPortalBlock;
 public final class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(JustEndPortal.MODID);
     public static final DeferredBlock<EndPortalGeneratorBlock> END_PORTAL_GENERATOR = BLOCKS.registerBlock("end_portal_generator", EndPortalGeneratorBlock::new, properties -> properties.destroyTime(5.0F).explosionResistance(1200.0F).sound(SoundType.STONE).lightLevel(state -> 15).noOcclusion());
-    public static final DeferredBlock<LinkedEndPortalBlock> LINKED_END_PORTAL = BLOCKS.registerBlock("linked_end_portal", LinkedEndPortalBlock::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.END_PORTAL));
+    public static final DeferredBlock<LinkedEndPortalBlock> LINKED_END_PORTAL = BLOCKS.registerBlock("linked_end_portal", LinkedEndPortalBlock::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.END_PORTAL).destroyTime(2.0F).sound(SoundType.GLASS));
 
     private ModBlocks() {}
 }
